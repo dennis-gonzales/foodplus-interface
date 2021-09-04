9e454483daf2ab21299a410d72785a3dbd093f83
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-09-05 07:54:13 +0800

    test

--- app/src/store/__tests__/counter.spec.ts
+++ app/src/store/__tests__/counter.spec.ts
@@ -1,7 +1,8 @@
+import { AnyAction, } from 'redux';
 import reducer, { increment, decrement, incrementByAmount } from '../features/counterSlice';
 
 test('should return the initial state', () => {
-  expect(reducer(undefined, {} as any)).toEqual({ value: 0 });
+  expect(reducer(undefined, {} as AnyAction)).toEqual({ value: 0 });
 });
 
 test('should handle increment', () => {
@@ -15,3 +16,47 @@ test('should handle decrement', () => {
 test('should handle incrementByAmount', () => {
   expect(reducer({ value: 0 }, incrementByAmount(5))).toEqual({ value: 5 });
 });
+
+type Person = {
+  name: string;
+  gender: "Female" | "Male" | "Others";
+  age: number;
+}
+
+// type User = {
+//   [key: string]: Person;
+//   test: Person;
+// }
+// const users: User = {
+//   p1,
+//   p2,
+// };
+
+// console.log(users['p1']);
+// console.log(users['p2']);
+
+
+const p1: Person = {
+  name: "Dennis",
+  gender: "Male",
+  age: 23
+}
+
+const p2: Person = {
+  name: "Joy",
+  gender: "Female",
+  age: 22,
+}
+
+const newUsers: Record<string, Person> = {
+  default: {
+    name: "default",
+    gender: "Others",
+    age: 0,
+  }
+};
+
+newUsers['p1'] = p1;
+newUsers['p2'] = p2;
+
+console.log(newUsers);
