59e1e8541d4bc8372d0128336e510dcced42c0f6
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-11-07 16:01:50 +0800

    change category and filer

--- app/src/views/containers/ProductListContainer.tsx
+++ app/src/views/containers/ProductListContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationProp, useNavigation } from '@react-navigation/core';
+import validator from 'validator';
 
 import { useAppDispatch, useAppSelector } from '../../core/hooks/storeApi';
 import { ScreenParamList } from '../../core/configs/routes';
@@ -8,9 +9,11 @@ import { addToCart } from '../../store/slices/cartSlice';
 
 import ProductList from '../components/organisms/ProductList';
 import {
+  filterProducts,
   loadProducts,
   selectFilterableProducts,
   selectProduct,
+  selectSearchTerm,
 } from '../../store/slices/productsSlice';
 import { selectActive } from '../../store/slices/categoriesSlice';
 
@@ -21,16 +24,24 @@ const ProductListContainer: React.FC = () => {
   const dispatch = useAppDispatch();
 
   const products = useAppSelector(selectFilterableProducts);
-  const category = useAppSelector(selectActive)
+  const category = useAppSelector(selectActive);
+  const searchTerm = useAppSelector(selectSearchTerm);
 
   React.useEffect(() => {
-    dispatch(
-      loadProducts({
-        category,
-      })
-    );
-  }, [category]);
+    const loadProductAndFilter = async () => {
+      await dispatch(
+        loadProducts({
+          category,
+        })
+      );
+      if (!validator.isEmpty(searchTerm)) {
+        dispatch(filterProducts(searchTerm));
+      }
 
+    };
+
+    loadProductAndFilter();
+  }, [category]);
 
   return (
     <ProductList
