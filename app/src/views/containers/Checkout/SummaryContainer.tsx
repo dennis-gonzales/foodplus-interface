c202603daf9138339f4dfae3edf4b74ac803a449
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-11-12 23:13:56 +0800

    summary container refactor

--- app/src/views/containers/Checkout/SummaryContainer.tsx
+++ app/src/views/containers/Checkout/SummaryContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import { NavigationProp, useNavigation } from '@react-navigation/core';
 
@@ -8,7 +9,7 @@ import CartItem from '../../../core/types/CartItem';
 
 import { selectProduct } from '../../../store/slices/productsSlice';
 import AddressCard from '../../components/molecules/AddressCard';
-import { selectCheckedProducts } from '../../../store/slices/cartSlice';
+import { clearCart, selectCheckedProducts } from '../../../store/slices/cartSlice';
 
 type SummaryContainerProps = NavigationProp<ScreenParamList, any>;
 
@@ -26,11 +27,52 @@ const SummaryContainer: React.FC = () => {
   return (
     <View style={styles.container}>
       <AddressCard />
+
+      <View style={styles.actions}>
+        <Button
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+          contentStyle={{
+            paddingVertical: 5,
+          }}
+          mode="outlined"
+        >
+          BACK
+        </Button>
+        <Button
+          style={styles.buyButton}
+          onPress={() => {
+            dispatch(clearCart());
+            navigation.navigate('OrderSuccess');
+          }}
+          contentStyle={{
+            paddingVertical: 5,
+          }}
+          mode="contained"
+        >
+          BUY
+        </Button>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  actions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  backButton: {
+    flex: 1,
+  },
+  buyButton: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     padding: 16,
